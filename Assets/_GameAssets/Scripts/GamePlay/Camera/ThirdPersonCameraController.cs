16ad658a1b67e692f380d7bb33d1bde854af77c8
Sarya-dev <sarya.cukur21@gmail.com>
2025-08-31 07:53:22 +0300

    v0.5 Camera Follow System Added

---
+++ Assets/_GameAssets/Scripts/GamePlay/Camera/ThirdPersonCameraController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _orientationTransform;
+    [SerializeField] private Transform _playervisualTransform;
+    [Header("Settings")]
+    [SerializeField] private float _rotationspeed;
+
+    private void Update()
+    {
+        Vector3 viewdirection = _playerTransform.position - new Vector3(transform.position.x, _playerTransform.position.y, transform.position.z);
+
+        _orientationTransform.forward = viewdirection.normalized;
+
+        float _verticalinput = Input.GetAxisRaw("Vertical");
+        float _horizontalinput = Input.GetAxisRaw("Horizontal");
+
+        Vector3 inputDirection = _orientationTransform.forward * _verticalinput + _orientationTransform.right * _horizontalinput;
+
+        if (inputDirection != Vector3.zero)
+        {
+
+
+            _playervisualTransform.forward
+            = Vector3.Slerp(_playervisualTransform.forward, inputDirection.normalized, Time.deltaTime * _rotationspeed);
+        }
+
+
+    }
+}
